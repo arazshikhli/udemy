f8a0cfe773a885ba2ac0b46c3cdc153246f3f3d1
Araz Shikhli <arazaraz777@gmail.com>
2022-07-19 17:30:38 +0400

    azadi oldur

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 document.getElementById("start-game").onclick=function(){
     document.getElementById("game-cont").style.display="block";
 }
+document.getElementById("krot-div").onclick=function(){
+    document.getElementById("krot-div").style.display="none";
+}
